2b39226b310d0eda465498b11d4d01ca8c9476ca
samieulhaq <msamiatuet@gmail.com>
2026-07-24 00:47:23 +0500

    styling of components and delete functionality

--- todoapp/src/components/Form.jsx
+++ todoapp/src/components/Form.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import styles from "./form.module.css";
 export default function Form({ todos, setTodos }) {
   const [todo, setTodo] = useState("");
   function handleSubmit(e) {
@@ -7,14 +8,19 @@ export default function Form({ todos, setTodos }) {
     setTodo("");
   }
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        onChange={(e) => setTodo(e.target.value)}
-        value={todo}
-        placeholder="Add a new todo..."
-      />
-      <button type="submit">Add</button>
+    <form className={styles.todoForm} onSubmit={handleSubmit}>
+      <div className={styles.inputContainer}>
+        <input
+          className={styles.todoInput}
+          type="text"
+          onChange={(e) => setTodo(e.target.value)}
+          value={todo}
+          placeholder="Add a new todo..."
+        />
+        <button className={styles.todoButton} type="submit">
+          Add
+        </button>
+      </div>
     </form>
   );
 }
--- todoapp/src/components/TodoList.jsx
+++ todoapp/src/components/TodoList.jsx
@@ -1,9 +1,10 @@
+import styles from "./todolist.module.css";
 import TodoItem from "./TodoItem";
-export default function TodoList({ todos }) {
+export default function TodoList({ todos, setTodos }) {
   return (
-    <div>
+    <div className={styles.list}>
       {todos.map((item) => (
-        <TodoItem key={item} item={item} />
+        <TodoItem key={item} item={item} todos={todos} setTodos={setTodos} />
       ))}
     </div>
   );
