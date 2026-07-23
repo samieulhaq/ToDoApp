dbe8c132c5eeed4fbce0eb8c59b9b6d759454b85
samieulhaq <msamiatuet@gmail.com>
2026-07-23 22:12:54 +0500

    Initial Commit

---
+++ todoapp/src/components/Todo.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import TodoItem from "./TodoItem";
+
+export default function Todo() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodos([...todos, todo]);
+    setTodo("");
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(e) => setTodo(e.target.value)}
+          value={todo}
+          placeholder="Add a new todo..."
+        />
+        <button type="submit">Add</button>
+      </form>
+      {todos.map((item) => (
+        <TodoItem key={item} item={item} />
+      ))}
+    </div>
+  );
+}
