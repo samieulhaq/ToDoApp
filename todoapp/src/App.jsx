04802d326e6dce525d36839d7ba581c44279fb76
samieulhaq <msamiatuet@gmail.com>
2026-07-23 23:33:45 +0500

    Header Styling and breaking down of components

--- todoapp/src/App.jsx
+++ todoapp/src/App.jsx
@@ -1,8 +1,11 @@
+import "./App.css";
+import Header from "./components/Header";
 import Todo from "./components/Todo";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Todo />
     </div>
   );
--- todoapp/src/components/Todo.jsx
+++ todoapp/src/components/Todo.jsx
@@ -1,28 +1,12 @@
 import { useState } from "react";
-import TodoItem from "./TodoItem";
-
+import Form from "./Form";
+import TodoList from "./TodoList";
 export default function Todo() {
-  const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
-  function handleSubmit(e) {
-    e.preventDefault();
-    setTodos([...todos, todo]);
-    setTodo("");
-  }
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          onChange={(e) => setTodo(e.target.value)}
-          value={todo}
-          placeholder="Add a new todo..."
-        />
-        <button type="submit">Add</button>
-      </form>
-      {todos.map((item) => (
-        <TodoItem key={item} item={item} />
-      ))}
+      <Form todos={todos} setTodos={setTodos} />
+      <TodoList todos={todos} />
     </div>
   );
 }
